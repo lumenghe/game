6cebcbe8c31d556392263bcf4d99444356f3f39b
lumenghe <lumenghe@gmail.com>
2020-08-28 09:57:15 +0200

    add function 'send_request'

--- main.py
+++ main.py
@@ -33,3 +33,32 @@ else:
     pass
 
 ###############################################################################
+
+def send_request(url, method, data = {}):
+    if method == 'get':
+        response = requests.get(url, params = data)
+    elif method == 'post':
+        response = requests.post(url, data = data)
+    else:
+        raise Exception('Bad method')
+
+    try:
+        content = json.loads(response.content.decode('utf-8'))
+    except:
+        raise Exception('Could not decode response')
+
+    if response.status_code == 200:
+        if 'error' in content:
+            if content['error'] == 'InvalidMove':
+                raise InvalidMoveException()
+            else:
+                raise Exception(content['error'])
+        else:
+            return content
+    else:
+        if 'error' in content:
+            raise Exception(content['error'])
+        else:
+            raise Exception('Unknown error')
+
+###############################################################################
