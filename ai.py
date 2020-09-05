cea0bd731a007a47e6185d8f2a4a85906e5afe9b
lumenghe <lumenghe@gmail.com>
2020-09-05 08:08:22 +0200

    import libs, adding 'inboard' function

--- ai.py
+++ ai.py
@@ -1,3 +1,10 @@
 """
     ai
 """
+import random
+import numpy as np
+from model import ValueNet
+
+def inboard(x, y, size=8):
+    """ check if (x,y) in board or not """
+    return (0 <= x < size) and (0 <= y < size)
