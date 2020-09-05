15b2b6ecafa290519a8ff10f85423359c3342b4c
lumenghe <lumenghe@gmail.com>
2020-09-05 08:08:26 +0200

    new function 'get_disc_type'

--- ai.py
+++ ai.py
@@ -8,3 +8,12 @@ from model import ValueNet
 def inboard(x, y, size=8):
     """ check if (x,y) in board or not """
     return (0 <= x < size) and (0 <= y < size)
+
+def get_disc_type(origin_type, x, size=8):
+    """ check disc type to king or not """
+    disc_type = origin_type
+    if origin_type == -1 and x == 0:
+        disc_type = -2
+    elif origin_type == 1 and x == size-1:
+        disc_type = 2
+    return disc_type
