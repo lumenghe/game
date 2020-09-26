2ce74cdb48a184f3f5f336fd67d97016092b1fa7
lumenghe <lumenghe@gmail.com>
2020-09-26 12:24:55 +0200

    adding function '__init__'

--- model.py
+++ model.py
@@ -6,3 +6,12 @@ import numpy as np
 
 class ValueNet():
     """policy-value network """
+    def __init__(self, model_filepath, generation, size=8, l2_penalty=1e-4):
+        self.size = size
+        self.l2_penalty = l2_penalty
+        self.model_filepath = model_filepath
+        self.generation = generation
+        if generation >= 0:
+            self.load_model()
+        else:
+            self.create_value_network()
