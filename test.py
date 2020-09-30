41b1df1d24da52d4aa0eaa99c02cd1466833a82f
lumenghe <lumenghe@gmail.com>
2020-09-30 09:53:10 +0200

    new file, minor modification

--- test.py
+++ test.py
@@ -8,3 +8,19 @@ def convert_board(size, board):
 
 def uniform_moves(moves):
     return [[list(m) for m in mm] for mm in moves]
+
+def check_moves(moves, ground_truth):
+    moves = uniform_moves(moves)
+    ground_truth = uniform_moves(ground_truth)
+    ok = True
+    for move in moves:
+        if move not in ground_truth:
+            print("FAILED: unexpected move: " + str(move))
+            ok = False
+    for move in ground_truth:
+        if move not in moves:
+            print("FAILED: missing move: " + str(move))
+            ok = False
+    if ok:
+        print("OK")
+    return ok
