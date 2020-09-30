49700747cff8d9f492b9e40c2100e346e3070777
lumenghe <lumenghe@gmail.com>
2020-09-30 09:36:00 +0200

    define function uniform_moves

--- test.py
+++ test.py
@@ -5,3 +5,6 @@ def convert_board(size, board):
     board = board.replace('\n', '')
     board = board.replace(' ', '')
     return [board[i:i + size] for i in range(0, len(board), size)]
+
+def uniform_moves(moves):
+    return [[list(m) for m in mm] for mm in moves]
